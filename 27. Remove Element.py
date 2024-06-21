1ced97e9e4c7d7e69a4f8ac278ef89d9292e7eae
ktikku <tikku_karan@lilly.com>
2024-06-22 01:07:49 +0530

    Day 5: Problem 10

---
+++ 27. Remove Element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                nums[i] = -1
+            else:
+                count += 1
+        nums = nums.sort(reverse = True)
+        return count
+        
