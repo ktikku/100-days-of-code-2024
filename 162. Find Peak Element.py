57a0651b5def058611b468ffb9341cf8de217468
ktikku <tikku_karan@lilly.com>
2024-07-03 22:06:52 +0530

    Day 17: Problem 39

---
+++ 162. Find Peak Element.py
@@ -0,0 +1,16 @@
+# Time Complexity: O(logn)
+# Space Complexity: O(1)
+# Approach: Use binary search to find the peak element.
+
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid - 1] <= nums[mid] >= nums[mid + 1]:
+                return mid
+            elif nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
