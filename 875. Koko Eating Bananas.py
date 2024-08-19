d377dbcecc1640992fa4161bcab4dd2731dcb2af
ktikku <tikku_karan@lilly.com>
2024-08-19 20:53:11 +0530

    Day 63: Problem 92

---
+++ 875. Koko Eating Bananas.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        def isSufficientSpeed(cnt):
+            return sum(ceil(i/cnt) for i in piles) <= h
+        while l < r:
+            m = (l + r)//2
+            if isSufficientSpeed(m):
+                r = m
+            else:
+                l = m + 1    
+        return l
